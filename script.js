4d96c1d23df7afb2e05e4ec02c1be3bbf496e317
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-03-02 16:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
-  // your code here
+ s=s.trim();
+ if(s.length===0)return '';
+ let wordArray=s.split(" ");
+ return wordArray[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
